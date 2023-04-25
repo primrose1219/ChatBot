473765f8e7a3354e1910316140160125102c9d2c
Moyomoto_ <harutoharut@hotmail.co.jp>
2023-04-25 15:23:36 +0900

    First commit

---
+++ DiscordBot/Main.cs
@@ -0,0 +1,10 @@
+﻿namespace DiscordBot
+{
+    internal class Dis
+    {
+        private static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
